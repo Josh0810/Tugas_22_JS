d4f0584b11c4d8c7e672c2b63d79a2fc8ae7d4c4
Josh0810 <92926113+Josh0810@users.noreply.github.com>
2022-01-04 23:45:10 +0700

    Add files via upload

---
+++ tugas22.js
@@ -0,0 +1,9 @@
+function Tugas22() {
+    var kalimat = "Saya Ingin Belajar Bersama";
+    var kata = kalimat.split(" ");
+    
+    return result = kata.forEach((item, index) => {
+        console.log("Item :", item, "Index ke", index);
+    })
+}
+Tugas22()
